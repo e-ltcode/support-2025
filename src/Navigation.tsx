2c1420560aaf25eed7d37701f08d4d12bf63c4ed
Djolini <e-ltcode@gmail.com>
2025-02-11 02:27:02 +0100

    Update navigation icons and add hover effects to nav links

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Link, NavLink, useNavigate } from "react-router-dom";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuestion, faSurprise, faUser, faUserFriends, faReply } from '@fortawesome/free-solid-svg-icons'
+import { faUserGear, faCircleQuestion, faComments, faRobot, faUser } from '@fortawesome/free-solid-svg-icons'
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -95,38 +95,34 @@ export function Navigation(props: INavigation) {
               }}
             >
               {isAuthenticated &&
-                // <NavLink to={`/supporter/0/${encodeURIComponent('Does Firefox support Manifest 3?')}/xyz`} className="nav-link"
                 <NavLink to={`/support-2025/supporter/0/${encodeURIComponent('radi bater')}/xyz`} className="nav-link" onClick={() => {
-                  //closeQuestionForm();
                   closeMenu();
                 }}>
-                  <FontAwesomeIcon icon={faSurprise} color='lightblue' />{' '}Supporter <small>(QA)</small>
+                  <FontAwesomeIcon icon={faUserGear} color='lightblue' />{' '}Supporter <small>(QA)</small>
                 </NavLink>
               }
               {isAuthenticated &&
                 <NavLink to="/support-2025/categories" className="nav-link" onClick={closeMenu}>
-                  <FontAwesomeIcon icon={faQuestion} color='lightblue' />{' '}Questions
+                  <FontAwesomeIcon icon={faCircleQuestion} color='lightblue' />{' '}Questions
                 </NavLink>
               }
               {isAuthenticated &&
                 <NavLink to="/support-2025/answers" className="nav-link" onClick={closeMenu}>
-                  <FontAwesomeIcon icon={faReply} color='lightblue' />{' '}Answers
+                  <FontAwesomeIcon icon={faComments} color='lightblue' />{' '}Answers
                 </NavLink>
               }
 
               {isAuthenticated && (ROLES.OWNER === enumRole || ROLES.ADMIN === enumRole) &&
                 <NavLink to="/support-2025/users" className="nav-link" onClick={closeMenu}>
-                  <FontAwesomeIcon icon={faUserFriends} color='lightblue' />{' '}Users
+                  <FontAwesomeIcon icon={faUserGear} color='lightblue' />{' '}Users
                 </NavLink>
               }
 
               {isAuthenticated &&
-                // <NavLink to={`/supporter/0/${encodeURIComponent('Does Firefox support Manifest 3?')}/xyz`} className="nav-link"
                 <NavLink to={`/support-2025/ChatBotPage/0/${encodeURIComponent('radi extension')}/xyz`} className="nav-link" onClick={() => {
-                  //closeQuestionForm();
                   closeMenu();
                 }}>
-                  <FontAwesomeIcon icon={faSurprise} color='lightblue' />{' '}ChatBot
+                  <FontAwesomeIcon icon={faRobot} color='lightblue' />{' '}ChatBot
                 </NavLink>
               }
 
@@ -135,13 +131,6 @@ export function Navigation(props: INavigation) {
                   About
                 </NavLink>
               }
-
-              {/* <NavDropdown
-                title={<><FontAwesomeIcon icon={faCog} color='lightblue' />{' '}Settings</>}
-                id={`offcanvasNavbarDropdown-expand`}
-                menuVariant={variant}
-              >
-              </NavDropdown> */}
 
               {!isAuthenticated &&
                 <NavLink to="/support-2025/register/fromNavigation/" className="nav-link" onClick={closeMenu}>
@@ -168,56 +157,18 @@ export function Navigation(props: INavigation) {
                   <NavDropdown.Item eventKey="LIGHT_MODE">
                     Light mode
                   </NavDropdown.Item>
-                  {/* <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form> */}
-
-                  <NavDropdown.Divider />
-                  {/* <NavDropdown
-                    title={<span style={{ padding: "0px 5px", fontSize: '0.9rem' }}><FontAwesomeIcon icon={faDatabase} />{' '}Local Storage</span>}
-                    id={`offcanvasNavbarDropdown-expand2`}
-                    menuVariant={variant}
-                    align="end"
-                  >
-                    <NavDropdown.Item href="#" eventKey="STORAGE_DISPLAY">
-                      Display
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#" eventKey="STORAGE_CLEAR">
-                      Clear
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#" eventKey="STORAGE_EXPORT">
-                      Export to zip file
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#" eventKey="STORAGE_IMPORT">
-                      Import from zip file
-                    </NavDropdown.Item>
-                  </NavDropdown> */}
-
                   <NavDropdown.Divider />
                   <NavDropdown.Item as={Link} to="/support-2025/export" onClick={closeMenu}>
                     Export Database to JSON
                   </NavDropdown.Item>
                   <NavDropdown.Divider />
-
                   <NavDropdown.Item as={Link} to="/support-2025/health" onClick={closeMenu}>
                     Health
                   </NavDropdown.Item>
-
                   <NavDropdown.Item as={Link} to="/support-2025/about" onClick={closeMenu}>
                     About
                   </NavDropdown.Item>
-
                   <NavDropdown.Divider />
-
                   <NavDropdown.Item href="#" onClick={() => { closeMenu(); otkaciMe(); }}>
                     Sign out
                   </NavDropdown.Item>
